3ec375ff75dd59262e43811418f688e0a8cfe779
CRW <cwirick@sightmachine.com>
2017-05-09 06:19:13 -0700

    Spelling error

--- factorytx/Config.py
+++ factorytx/Config.py
@@ -242,7 +242,7 @@ class Config(dict):
             if x['name'] not in outgoing_set:
                 log.error("There was no outgoing tx specified for the datasource %s", x['name'])
                 cfg_errors_count += 1
-        for x in all_coutgoing_data:
+        for x in all_outgoing_data:
             if x['name'] not in incoming_set:
                 log.error("There is no incoming dataplugin specified for the tx datasource %s.", x['name'])
                 cfg_errors_count += 1
